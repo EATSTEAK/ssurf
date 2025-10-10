6aacb4489a4c57b3965c97f4376b032cf3144e06
Koo Hyomin <me@eatsteak.dev>
2025-10-10 22:25:09 +0900

    feat: init `@rusaint/react-native`

---
+++ index.tsx
@@ -0,0 +1,11 @@
+import { registerRootComponent } from "expo";
+import "expo-dev-client";
+import { ExpoRoot } from "expo-router";
+
+// Must be exported or Fast Refresh won't update the context
+export function App() {
+  const ctx = require.context("./app");
+  return <ExpoRoot context={ctx} />;
+}
+
+registerRootComponent(App);
