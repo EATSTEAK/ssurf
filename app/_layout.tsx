447db10295db4bd3e5ddab02f2795693bc52f57a
Koo Hyomin <me@eatsteak.dev>
2025-10-12 01:10:28 +0900

    feat(db): db 초기화 및 마이그레이션 훅

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,29 @@
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
 import { Stack } from 'expo-router';
+import { Text, View } from 'react-native';
 
 import { RusaintProvider } from '@/components/providers/RusaintProvider';
+import { db } from '@/db';
+import migrations from '@/drizzle/migrations';
 
 export default function RootLayout() {
+  const { success, error } = useMigrations(db, migrations);
+  if (error) {
+    return (
+      <View>
+        <Text>{error.message}</Text>
+      </View>
+    );
+  }
+
+  if (!success) {
+    return (
+      <View>
+        <Text>Running migrations...</Text>
+      </View>
+    );
+  }
+
   return (
     <RusaintProvider>
       <Stack />
