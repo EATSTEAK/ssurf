d05c357004ad937e1d551405e4848cf3cf63197d
fecapark <fecapark@gmail.com>
2025-10-12 04:57:04 +0900

    feat: 더미 로그인 뷰 구현

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
-import { SemesterType } from '@rusaint/react-native';
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
 
-import { useGeneralChapelInformation } from '@/hooks/chapel/chapel';
+import { useRusaintSession } from '@/components/providers/RusaintSessionProvider';
 
 export default function Index() {
-  const result = useGeneralChapelInformation(2025, SemesterType.Two);
+  const {
+    login,
+    logout,
+    user: { session, id: userId, password: userPassword },
+  } = useRusaintSession();
+
+  const [id, setId] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const onLogin = async () => {
+    await login(id, password);
+  };
 
   return (
     <View
@@ -14,7 +25,25 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>{JSON.stringify(result)}</Text>
+      {session ? (
+        <>
+          <Text>{userId}</Text>
+          <Text>{userPassword}</Text>
+          <Text>{JSON.stringify(session)}</Text>
+          <Button onPress={() => logout()} title="logout" />
+        </>
+      ) : (
+        <>
+          <TextInput onChangeText={setId} placeholder="아무거나 입력하세요..." value={id} />
+          <TextInput
+            onChangeText={setPassword}
+            placeholder="아무거나 입력하세요..!"
+            secureTextEntry
+            value={password}
+          />
+          <Button onPress={onLogin} title="login" />
+        </>
+      )}
     </View>
   );
 }
