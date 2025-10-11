e80384137c2e1fe5ba7a335250b9747309568e78
Koo Hyomin <me@eatsteak.dev>
2025-10-12 00:10:32 +0900

    refactor: 상태 변경 간 컨텍스트 스위칭이 없도록 개선

--- components/providers/RusaintProvider.tsx
+++ components/providers/RusaintProvider.tsx
@@ -31,10 +31,11 @@ export const RusaintProvider = ({ children }: { children: React.ReactNode }) => 
       // TODO: retrieve ID/PW from secure storage
       // TODO: handle session refresh
       const ses = await new USaintSessionBuilder().withPassword('20211561', '<redacted>');
-      setSession(ses);
+      // NOTE: 각 애플리케이션을 생성하는 것은 서버(u-saint) 입장에서 탭을 하나 띄우는 것과 동일하므로, 동시에 요청하지 않고 순차적으로 요청해요
       const chapel = await new ChapelApplicationBuilder().build(ses);
-      setChapelClient(chapel);
       const courseSchedule = await new CourseScheduleApplicationBuilder().build(ses);
+      setSession(ses);
+      setChapelClient(chapel);
       setCourseScheduleClient(courseSchedule);
     })();
   }, []);
