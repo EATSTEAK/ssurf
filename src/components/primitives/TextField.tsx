2894a18103450a2f8b4c56dc21e1dc053d1b9c8d
Sanghyeok Park <fecapark@gmail.com>
2025-10-22 21:55:39 +0900

    [SUF-30] feat: TextField 컴포넌트 구현 (#49)

---
+++ src/components/primitives/TextField.tsx
@@ -0,0 +1,29 @@
+import { TextInput, TextInputProps } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+
+type Props = TextInputProps;
+
+export const TextField = ({ style, placeholderTextColor, ...props }: Props) => {
+  return (
+    <TextInput
+      {...props}
+      placeholderTextColor={placeholderTextColor ?? styles.placeholder.color}
+      style={[styles.container, style]}
+    />
+  );
+};
+
+const styles = StyleSheet.create((theme) => ({
+  container: {
+    width: '100%',
+    height: 48,
+    paddingHorizontal: 12,
+    borderRadius: 12,
+    backgroundColor: theme.colors.backgroundCard,
+    color: theme.colors.foregroundMuted,
+    fontWeight: 500,
+  },
+  placeholder: {
+    color: theme.colors.foregroundMuted,
+  },
+}));
