39016b2638230a8bbcf3eb3f3ad3ece101003a1f
Koo Hyomin <me@eatsteak.dev>
2025-11-13 00:42:23 +0900

    feat(chapel): add padding in seatmap view

--- src/components/chapel/ChapelSeatmapView.tsx
+++ src/components/chapel/ChapelSeatmapView.tsx
@@ -19,6 +19,7 @@ const styles = StyleSheet.create((theme) => ({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+    padding: theme.gap(1),
   },
   firstFloor: {
     aspectRatio: 915 / 594,
