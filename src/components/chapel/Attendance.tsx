cf4652ff452dd2a32c622214a54a2b4534ea37cd
Koo Hyomin <me@eatsteak.dev>
2025-11-12 21:31:50 +0900

    feat(chapel): `Attendance` component

--- src/components/chapel/Attendance.tsx
+++ src/components/chapel/Attendance.tsx
@@ -7,7 +7,7 @@ import { ChapelAttendanceDto } from '@/db/schema/chapel';
 const styles = StyleSheet.create((theme) => ({
   container: {
     padding: 10,
-    backgroundColor: theme.colors.primaryContainer,
+    backgroundColor: theme.colors.surfaceDim,
     borderRadius: theme.cornerRadius.md,
     display: 'flex',
     gap: theme.gap(2),
@@ -18,11 +18,11 @@ const styles = StyleSheet.create((theme) => ({
 export const Attendance = ({ attendance }: { attendance: ChapelAttendanceDto }) => {
   return (
     <View key={attendance.date} style={styles.container}>
-      <ThemedText typography="headingMd">
+      <ThemedText color="surfaceDim" typography="headingMd">
         {attendance.category} (
         {`${attendance.instructor} ${attendance.instructorDepartment}`.trim()})
       </ThemedText>
-      <ThemedText style={{ alignSelf: 'flex-end' }}>
+      <ThemedText color="surfaceDim" style={{ alignSelf: 'flex-end' }}>
         {attendance.date} / {attendance.attendance}
       </ThemedText>
     </View>
