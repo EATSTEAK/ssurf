5df471df44f27249d7cdbc180e13015aeb240498
Koo Hyomin <me@eatsteak.dev>
2025-11-13 18:31:08 +0900

    feat(chapel): improve attendance layouts

--- src/components/chapel/Attendance.tsx
+++ src/components/chapel/Attendance.tsx
@@ -9,21 +9,61 @@ const styles = StyleSheet.create((theme) => ({
     padding: 10,
     backgroundColor: theme.colors.surfaceDim,
     display: 'flex',
-    gap: theme.gap(2),
+    flexDirection: 'column',
     width: '100%',
+    gap: theme.gap(1),
+  },
+  categoryContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    gap: theme.gap(1),
+    flexWrap: 'wrap',
+  },
+  chip: {
+    variants: {
+      attendance: {
+        attended: {
+          backgroundColor: theme.colors.success,
+        },
+        absent: {
+          backgroundColor: theme.colors.error,
+        },
+        pending: {
+          backgroundColor: theme.colors.surfaceDimmer,
+        },
+      },
+    },
+    borderRadius: theme.cornerRadius.md,
+    paddingVertical: theme.gap(0.5),
+    paddingHorizontal: theme.gap(1),
+    alignSelf: 'flex-end',
   },
 }));
 
 export const Attendance = ({ attendance }: { attendance: ChapelAttendanceDto }) => {
+  styles.useVariants({
+    attendance:
+      attendance.attendance === '출석'
+        ? 'attended'
+        : attendance.attendance === '결석'
+          ? 'absent'
+          : 'pending',
+  });
   return (
     <View key={attendance.date} style={styles.container}>
-      <ThemedText color="fgSurfaceDim" typography="headingMd">
-        {attendance.category} (
-        {`${attendance.instructor} ${attendance.instructorDepartment}`.trim()})
-      </ThemedText>
-      <ThemedText color="fgSurfaceDim" style={{ alignSelf: 'flex-end' }}>
-        {attendance.date} / {attendance.attendance || '미결'}
-      </ThemedText>
+      <View style={styles.categoryContainer}>
+        <ThemedText color="fgSurfaceDim" typography="labelMd">
+          {attendance.category}
+        </ThemedText>
+        <ThemedText color="fgSurfaceDim" style={{ flexGrow: 1 }} typography="headingMd">
+          {`${attendance.instructor} ${attendance.instructorDepartment}`.trim()}
+        </ThemedText>
+        <ThemedText color="fgSuccessContainer" style={styles.chip} typography="labelMd">
+          {attendance.date} / {attendance.attendance || '미결'}
+        </ThemedText>
+      </View>
     </View>
   );
 };
