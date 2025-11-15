2ecde82443d71a689962eff650e3071692afe71c
Koo Hyomin <me@eatsteak.dev>
2025-11-16 02:40:44 +0900

    feat(hooks): grade sync hooks

--- src/components/providers/RusaintApplicationProvider.tsx
+++ src/components/providers/RusaintApplicationProvider.tsx
@@ -1,6 +1,8 @@
 import {
   ChapelApplicationBuilder,
   ChapelApplicationInterface,
+  CourseGradesApplicationBuilder,
+  CourseGradesApplicationInterface,
   CourseScheduleApplicationBuilder,
   CourseScheduleApplicationInterface,
 } from '@rusaint/react-native';
@@ -11,11 +13,13 @@ import { useRusaintSession } from '@/components/providers/RusaintSessionProvider
 export interface RusaintApplicationContext {
   chapelClient: ChapelApplicationInterface | null;
   courseScheduleClient: CourseScheduleApplicationInterface | null;
+  gradesClient: CourseGradesApplicationInterface | null;
 }
 
 const RusaintApplicationContext = createContext<RusaintApplicationContext>({
   chapelClient: null,
   courseScheduleClient: null,
+  gradesClient: null,
 });
 
 export const RusaintApplicationProvider = ({ children }: React.PropsWithChildren<unknown>) => {
@@ -24,6 +28,7 @@ export const RusaintApplicationProvider = ({ children }: React.PropsWithChildren
   const [chapelClient, setChapelClient] = useState<ChapelApplicationInterface | null>(null);
   const [courseScheduleClient, setCourseScheduleClient] =
     useState<CourseScheduleApplicationInterface | null>(null);
+  const [gradesClient, setGradesClient] = useState<CourseGradesApplicationInterface | null>(null);
 
   useAsyncEffect(async () => {
     if (!session) {
@@ -32,8 +37,10 @@ export const RusaintApplicationProvider = ({ children }: React.PropsWithChildren
     // NOTE: 각 애플리케이션을 생성하는 것은 서버(u-saint) 입장에서 탭을 하나 띄우는 것과 동일하므로, 동시에 요청하지 않고 순차적으로 요청해요
     const chapel = await new ChapelApplicationBuilder().build(session);
     const courseSchedule = await new CourseScheduleApplicationBuilder().build(session);
+    const grades = await new CourseGradesApplicationBuilder().build(session);
     setChapelClient(chapel);
     setCourseScheduleClient(courseSchedule);
+    setGradesClient(grades);
   }, [session]);
 
   return (
@@ -41,6 +48,7 @@ export const RusaintApplicationProvider = ({ children }: React.PropsWithChildren
       value={{
         chapelClient,
         courseScheduleClient,
+        gradesClient,
       }}
     >
       {children}
