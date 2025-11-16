c6b2672000452cac73c647d60fd209e345188f5b
Koo Hyomin <me@eatsteak.dev>
2025-11-16 14:55:39 +0900

    chore: install `expo-drizzle-studio-plugin` to examine db

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import { registerRootComponent } from 'expo';
 import 'expo-dev-client';
+import { useDrizzleStudio } from 'expo-drizzle-studio-plugin';
 import { ExpoRoot } from 'expo-router';
 
+import { expoDb } from '@/db';
 import '@/unistyles';
 
 export const REV: string = process.env.EXPO_PUBLIC_REV ?? 'dev';
 
 // Must be exported or Fast Refresh won't update the context
 export function App() {
+  useDrizzleStudio(expoDb);
   const ctx = require.context('./app');
   return <ExpoRoot context={ctx} />;
 }
