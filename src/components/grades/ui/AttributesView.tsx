2b9a1103805713073bfd4c8f651b44d403751fda
Koo Hyomin <me@eatsteak.dev>
2025-12-22 21:26:41 +0900

    feat(grades): AttributesView ui

---
+++ src/components/grades/ui/AttributesView.tsx
@@ -0,0 +1,38 @@
+import { View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+
+import { ThemedText } from '@/components/primitives/ThemedText';
+
+const styles = StyleSheet.create((theme) => ({
+  summaryView: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: theme.gap(1),
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  attributeView: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: theme.gap(0.5),
+    minWidth: '33%',
+    flexGrow: 1,
+  },
+}));
+
+export function AttributesView({ items }: { items: { label: string; value: number | string }[] }) {
+  return (
+    <View style={styles.summaryView}>
+      {items.map((item) => (
+        <View key={item.label} style={styles.attributeView}>
+          <ThemedText style={{ color: 'gray' }} typography="bodyMd">
+            {item.label}
+          </ThemedText>
+          <ThemedText style={{ fontWeight: 600 }} typography="bodyLg">
+            {item.value}
+          </ThemedText>
+        </View>
+      ))}
+    </View>
+  );
+}
