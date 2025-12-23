99f1d4bc1ecc35d8b6bc5467c89f5f25b8e8a781
Koo Hyomin <me@eatsteak.dev>
2025-12-23 15:55:31 +0900

    feat(primitives/chip): custom color and bgColor

--- src/components/primitives/Chip.tsx
+++ src/components/primitives/Chip.tsx
@@ -1,25 +1,33 @@
 import { View, ViewStyle } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
-import { ThemedText } from '@/components/primitives/ThemedText';
+import { ThemedText, ThemedTextProps } from '@/components/primitives/ThemedText';
 
 const styles = StyleSheet.create((theme) => ({
-  chip: {
+  chip: (backgroundColor: keyof typeof theme.colors) => ({
     borderRadius: theme.cornerRadius.md,
     paddingVertical: theme.gap(0.5),
     paddingHorizontal: theme.gap(1),
-  },
+    backgroundColor: theme.colors[backgroundColor],
+  }),
 }));
 
 interface ChipProps {
+  backgroundColor?: Parameters<typeof styles.chip>[0];
   children: React.ReactNode;
+  color?: ThemedTextProps['color'];
   style?: ViewStyle;
 }
 
-export const Chip = ({ children, style }: ChipProps) => {
+export const Chip = ({
+  children,
+  style,
+  color = 'fgPrimaryContainer',
+  backgroundColor = 'primaryContainer',
+}: ChipProps) => {
   return (
-    <View style={[styles.chip, style]}>
-      <ThemedText color="fgSuccessContainer" typography="labelMd">
+    <View style={[styles.chip(backgroundColor), style]}>
+      <ThemedText color={color} typography="labelMd">
         {children}
       </ThemedText>
     </View>
