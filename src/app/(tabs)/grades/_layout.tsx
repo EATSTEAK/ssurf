6a79264e1e9248ba7993c6eeea3c9313b767efc5
Koo Hyomin <me@eatsteak.dev>
2026-01-03 01:11:12 +0900

    feat(app/grades): add graduation screen

--- src/app/(tabs)/grades/_layout.tsx
+++ src/app/(tabs)/grades/_layout.tsx
@@ -1,9 +1,22 @@
 import { Stack } from 'expo-router';
+import { StyleSheet } from 'react-native-unistyles';
+
+const styles = StyleSheet.create((theme) => ({
+  background: {
+    backgroundColor: theme.colorsHex.surface,
+  },
+}));
 
 export default function StackLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: styles.background.backgroundColor },
+      }}
+    >
       <Stack.Screen name="index" />
+      <Stack.Screen name="graduation" />
     </Stack>
   );
 }
---
+++ src/app/(tabs)/grades/graduation.tsx
@@ -0,0 +1,140 @@
+import { Image } from 'expo-image';
+import { Stack } from 'expo-router';
+import { Platform, View } from 'react-native';
+import { StyleSheet } from 'react-native-unistyles';
+
+import errorImage from '@/assets/error.png';
+import loadingImage from '@/assets/loading.png';
+import { useGraduationView } from '@/features/grades/lib/useGraduationView';
+import { SafeContainer } from '@/shared/ui/containers/Container';
+import { RefreshableScrollView } from '@/shared/ui/containers/RefreshableScrollView';
+import { Header } from '@/shared/ui/headers/Header';
+import { Space } from '@/shared/ui/primitives/Space';
+import { ThemedText } from '@/shared/ui/primitives/ThemedText';
+
+const styles = StyleSheet.create((theme) => ({
+  root: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: theme.colors.surface,
+    position: 'relative',
+  },
+
+  topView: {
+    width: '100%',
+    display: 'flex',
+    gap: theme.gap(1),
+    flexDirection: 'column',
+    padding: theme.gap(3),
+  },
+  errorView: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    gap: 16,
+    marginBottom: 96,
+  },
+  imageView: {
+    width: 150,
+    height: 150,
+    marginBottom: theme.gap(2),
+  },
+  eyeIcon: {
+    size: 16,
+    color: theme.colorsHex.fgSurface,
+  },
+  eyeOffIcon: {
+    size: 16,
+    color: theme.colorsHex.fgSurfaceMuted,
+  },
+}));
+
+function GraduationContent() {
+  const { data, error, isLoading, refresh } = useGraduationView();
+
+  const handleErrorRefresh = async () => {
+    // 로딩 중이면 리프레시하지 않음
+    if (isLoading) {
+      return;
+    }
+    await refresh();
+  };
+
+  if (!data) {
+    return (
+      <View style={styles.root}>
+        <RefreshableScrollView onRefresh={handleErrorRefresh} refreshing={isLoading}>
+          <SafeContainer>
+            {Platform.OS === 'ios' && <Space gap={2} />}
+            <View style={styles.topView}>
+              <Header title="졸업" />
+            </View>
+            <Space gap={1} />
+            <View style={styles.errorView}>
+              {error ? (
+                <>
+                  <Image contentFit="contain" source={errorImage} style={styles.imageView} />
+                  <ThemedText color="error" typography="headingLg">
+                    정보를 가져오는 중 오류가 발생했어요.
+                  </ThemedText>
+                  <ThemedText typography="bodyLg">아래로 당겨 다시 시도해보세요.</ThemedText>
+                  <ThemedText typography="bodySm">{error?.message}</ThemedText>
+                </>
+              ) : (
+                <>
+                  <Image contentFit="contain" source={loadingImage} style={styles.imageView} />
+                  <ThemedText typography="headingLg">정보를 가져오는 중이에요.</ThemedText>
+                  <ThemedText typography="bodyLg">잠시만 기다려주세요.</ThemedText>
+                </>
+              )}
+            </View>
+          </SafeContainer>
+        </RefreshableScrollView>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.root}>
+      <RefreshableScrollView onRefresh={handleErrorRefresh} refreshing={isLoading}>
+        <SafeContainer>
+          {Platform.OS === 'ios' && <Space gap={2} />}
+          <View style={styles.topView}>
+            <Header title="졸업" />
+          </View>
+          <Space gap={1} />
+          <View>
+            {data.requirements.map((requirement) => (
+              <ThemedText key={requirement.id} typography="bodyMd">
+                {requirement.name}
+                {requirement.requirement && (
+                  <>
+                    : {requirement.calculation} / {requirement.requirement} 학점
+                  </>
+                )}
+              </ThemedText>
+            ))}
+          </View>
+        </SafeContainer>
+      </RefreshableScrollView>
+    </View>
+  );
+}
+
+export default function Graduation() {
+  return (
+    <>
+      <Stack.Screen
+        options={{
+          headerShown: true,
+          headerTransparent: true,
+          title: '졸업',
+          headerTitle: () => <></>,
+          headerBackVisible: true,
+        }}
+      />
+      <GraduationContent />
+    </>
+  );
+}
