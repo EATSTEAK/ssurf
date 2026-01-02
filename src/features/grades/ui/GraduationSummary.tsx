77c397de02012bad0d5e6618954b7530297e22df
Koo Hyomin <me@eatsteak.dev>
2026-01-03 01:11:29 +0900

    feat(features/grades): add button to gradation screen

--- src/features/grades/ui/GraduationSummary.tsx
+++ src/features/grades/ui/GraduationSummary.tsx
@@ -1,6 +1,8 @@
+import { useRouter } from 'expo-router';
 import { View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 
+import { ArrowForwardIcon } from '@/shared/ui/icons';
 import { Button } from '@/shared/ui/primitives/Button';
 import { ThemedText } from '@/shared/ui/primitives/ThemedText';
 
@@ -8,9 +10,24 @@ const styles = StyleSheet.create((theme) => ({
   container: {
     gap: theme.gap(1),
   },
+  icon: {
+    color: theme.colorsHex.fgSurface,
+  },
+  buttonStyle: ({ pressed }) => ({
+    backgroundColor: pressed ? theme.colors.surfaceDimmer : theme.colors.surfaceDim,
+    alignItems: 'flex-start',
+    paddingHorizontal: theme.gap(2),
+  }),
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: theme.gap(0.5),
+  },
 }));
 
 export const GraduationSummary = () => {
+  const { navigate } = useRouter();
   return (
     <View style={styles.container}>
       <View>
@@ -18,11 +35,21 @@ export const GraduationSummary = () => {
         <ThemedText color="primaryInverted" typography="heading3xl">
           {(100).toFixed(1)}
           <ThemedText color="fgSurfaceMuted" typography="labelLg">
-            / 133.00
+            {' '}
+            / 133.0
           </ThemedText>
         </ThemedText>
       </View>
-      <Button>졸업사정표 보기</Button>
+      <Button
+        onPress={() => navigate('/(tabs)/grades/graduation')}
+        style={styles.buttonStyle}
+        variant="ghost"
+      >
+        <View style={styles.buttonContent}>
+          <ArrowForwardIcon color={styles.icon.color} size={16} />
+          <ThemedText>상세 보기</ThemedText>
+        </View>
+      </Button>
     </View>
   );
 };
