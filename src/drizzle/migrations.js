fe108f7db208a9046023a93675f061629f1a4a36
Koo Hyomin <me@eatsteak.dev>
2026-01-04 22:50:37 +0900

    feat: generate drizzle migrations

--- src/drizzle/migrations.js
+++ src/drizzle/migrations.js
@@ -8,6 +8,7 @@ import m0003 from './0003_busy_husk.sql';
 import m0004 from './0004_huge_pretty_boy.sql';
 import m0005 from './0005_foamy_goliath.sql';
 import m0006 from './0006_calm_wasp.sql';
+import m0007 from './0007_uneven_grey_gargoyle.sql';
 
   export default {
     journal,
@@ -18,7 +19,8 @@ m0002,
 m0003,
 m0004,
 m0005,
-m0006
+m0006,
+m0007
     }
   }
   
