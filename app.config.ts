e91714549bcfd1b89afe10b63069fdb340ec29ef
Hyomin Koo <me@eatsteak.dev>
2026-04-06 13:24:53 +0900

    release: b2026040601

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ const name = IS_DEV ? 'SSURF (dev)' : 'SSURF';
 
 const bundleIdentifier = IS_DEV ? 'dev.eatsteak.ssurf.dev' : 'dev.eatsteak.ssurf';
 
-const versionCode = 2026040501;
+const versionCode = 2026040601;
 
 const config: ExpoConfig = {
   name,
